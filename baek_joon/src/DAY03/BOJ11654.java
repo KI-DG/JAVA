fe22aae55d3f4fa0e6069b065d75392832be7a99
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-06-18 18:22:47 +0900

    [FEAT] BOJ11654_문제해결

---
+++ baek_joon/src/DAY03/BOJ11654.java
@@ -0,0 +1,14 @@
+package DAY03;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        char textA = a.charAt(0);
+
+        System.out.println((int)textA);
+    }
+}
