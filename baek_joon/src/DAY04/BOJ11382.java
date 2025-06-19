156a7b02b2ffb81f9f4503c307ea9364c2f94da8
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-06-19 23:15:43 +0900

    [FEAT] BOJ11382_문제해결

---
+++ baek_joon/src/DAY04/BOJ11382.java
@@ -0,0 +1,18 @@
+package DAY04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class BOJ11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(a + b + c);
+    }
+}
