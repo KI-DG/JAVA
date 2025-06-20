713eef250fa6229218b7ef3b13790b7fb54b18b5
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-06-21 02:38:03 +0900

    [FEAT] BOJ9086_문제해결

---
+++ baek_joon/src/DAY05/BOJ9086.java
@@ -0,0 +1,17 @@
+package DAY05;
+
+import java.util.Scanner;
+
+public class BOJ9086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String word = sc.next();
+            String answerOne = word.substring(0,1);
+            String answerTwo = word.substring(word.length() - 1);
+            System.out.println(answerOne + answerTwo);
+        }
+    }
+}
