e9e69992c3809e5e9f93c8c1b7fee6fb5fe76b99
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-06-21 02:35:55 +0900

    [FEAT] BOJ10807_문제해결

---
+++ baek_joon/src/DAY05/BOJ10807.java
@@ -0,0 +1,26 @@
+package DAY05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<Integer> nums = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            nums.add(a);
+        }
+        int num = sc.nextInt();
+        for (int b : nums) {
+            if (b == num) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
