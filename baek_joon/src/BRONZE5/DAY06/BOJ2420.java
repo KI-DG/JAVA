ac4a2a71619ff924a0ddd8bf3fd026f81eb42b5a
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-06-22 01:26:43 +0900

    [FEAT] BOJ2420_문제해결

---
+++ baek_joon/src/BRONZE5/DAY06/BOJ2420.java
@@ -0,0 +1,14 @@
+package BRONZE5.DAY06;
+
+import java.util.Scanner;
+
+public class BOJ2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        System.out.println(Math.abs(n - m));
+    }
+}
