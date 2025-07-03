942b581bd913f4b80c62cc242eb10172964e646e
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-07-03 17:07:39 +0900

    [FEAT] BOJ24263_문제해결

---
+++ baek_joon/src/BRONZE4/DAY04/BOJ24263.java
@@ -0,0 +1,14 @@
+package BRONZE4.DAY04;
+
+import java.util.Scanner;
+
+public class BOJ24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
