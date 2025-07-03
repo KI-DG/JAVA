91a6cf20e9d73f89e05b80c3da91b268971d02cc
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-07-03 17:08:31 +0900

    [FEAT] BOJ9316_문제해결

---
+++ baek_joon/src/BRONZE4/DAY04/BOJ9316.java
@@ -0,0 +1,15 @@
+package BRONZE4.DAY04;
+
+import java.util.Scanner;
+
+public class BOJ9316 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n + 1; i++) {
+            System.out.println("Hello World, Judge " + i +"!");
+        }
+    }
+}
