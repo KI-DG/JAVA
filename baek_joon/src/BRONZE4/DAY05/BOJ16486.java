85bb043a4039095e02fea64bfd2a1262a02c9643
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-07-28 17:15:24 +0900

    [FEAT] BOJ16486_문제해결

---
+++ baek_joon/src/BRONZE4/DAY05/BOJ16486.java
@@ -0,0 +1,14 @@
+package BRONZE4.DAY05;
+
+import java.util.Scanner;
+
+public class BOJ16486 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(2 * A + 2 * B * 3.141592);
+    }
+}
