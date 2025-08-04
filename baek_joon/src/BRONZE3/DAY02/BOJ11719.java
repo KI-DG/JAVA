744a4dd9d73d473cae617ae7b639fd9e60cb106c
LEE KI YONG <109258291+KI-DG@users.noreply.github.com>
2025-08-04 15:36:38 +0900

    [FEAT] BOJ11719_문제해결

---
+++ baek_joon/src/BRONZE3/DAY02/BOJ11719.java
@@ -0,0 +1,17 @@
+package BRONZE3.DAY02;
+
+import java.util.Scanner;
+
+public class BOJ11719 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String text = sc.nextLine();
+
+            System.out.println(text);
+
+        }
+
+    }
+}
